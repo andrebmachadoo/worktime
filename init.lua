b08b6bcbe04bbdf9969f5da9941991d45e76dee8
andrebmachadoo <andrebmachadoo@gmail.com>
2024-08-16 14:34:37 -0300

    debug

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@ local M = {}
 local config = require('config')
 local commands = require('commands')
 
+print(config)
+print(commands)
+
 function M.setup(user_config)
   config.setup(user_config)
   
